5680202f03dfdaeb4e919f585dd8e13f09fb3583
Peter Selch Dahl - Azure MVP <peterselchdahl@gmail.com>
2019-04-18 23:39:46 +0200

    Without the variable
    Please see the support case "[Harvest Support] "Support for unicode chars in the API?"". The output does not support special chars when using Invoke-WebRequest. For some reason it uses the wrong encoding.
    
    https://social.technet.microsoft.com/Forums/Lync/en-US/d795e7d2-dcf1-4323-8e06-8f06ce31a897/bug-invokerestmethod-and-utf8-data?forum=winserverpowershell
    
    /Peter Selch Dahl

--- v2/harvest_api_sample.ps1
+++ v2/harvest_api_sample.ps1
@@ -1,6 +1,7 @@
-$Url = "https://api.harvestapp.com/v2/users/me"
+$Url = "https://api.harvestapp.com/v2/invoices/19870387"
+#$Url = "https://api.harvestapp.com/v2/users/me"
 $Headers = @{}
-$Headers.Add("Authorization", "Bearer " + $Env:HARVEST_ACCESS_TOKEN)
-$Headers.Add("Harvest-Account-ID", $Env:HARVEST_ACCOUNT_ID)
+$Headers.Add("Authorization", "Bearer " + "1855912.pt.Rn8KaEQVc8FdOiC3hETUA1LPx-YqsA9yHrXIqpFfMVsqDDt_9NAQkIYu6ux9E0dqxMgwJpj5PkklL1o1Xxiz8g")
+$Headers.Add("Harvest-Account-ID", "1058613")
 
-Invoke-WebRequest -Uri $Url -Headers $Headers | ConvertFrom-Json
+[system.Text.Encoding]::UTF8.GetString((Invoke-WebRequest -Uri $Url -Headers $Headers).RawContentStream.ToArray()) | ConvertFrom-Json 
